d1fcc7804febc25035b0280905edc62b64912990
Mahabub Hossain <mahabubrishad@gmail.com>
2024-02-04 23:27:25 +0600

    changes new one

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,14 +30,13 @@ return [
 
     'disks' => [
 
-        'admin' => [ 
-            'driver' => 'local', 
-            'root' => storage_path('app/public'), 
-            'url' => env('APP_URL').'/storage', 
+        'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public'
         ],
-        
-        
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
